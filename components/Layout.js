09b4a93a79e246325be37673b3654b19705c9e07
Brayan Styvison <styvisonviana@gmail.com>
2023-08-11 21:33:03 -0300

    top-left-img

--- components/Layout.js
+++ components/Layout.js
@@ -13,7 +13,8 @@ import TopLeftImg from '../components/TopLeftImg'
 
 const Layout = ({children}) => {
   return (
-    <div className={'page bg-site text-white bg-cover bg-no-repeat'}>
+    <div 
+      className={'page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative'}>
       <TopLeftImg/>
       <Nav/>
       <Header/>
