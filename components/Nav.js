ce9dcf8dddd0f5901eb90253c6f60981e719731e
Brayan Styvison <styvisonviana@gmail.com>
2023-08-15 18:13:58 -0300

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -36,14 +36,21 @@ import {useRouter} from 'next/router'
 const Nav = () => {
   const router = useRouter()
   const pathname = router.pathname
-  return <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen bg-red-500/30 p-20 '> 
+  return (
+  <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'> 
     {/*inner*/}
-    <div className='flex w-full xl:flex-col  items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-80 xl:h-max py-8 bg-white/10 backdrop-blur-sn text-3xl xl:text-xl xl:rounded-full' >
-      {navData.map((link, index) => {
-        <Link href={link.path}>{link.icon}</Link>
-      })}
+    <div className='flex w-full xl:flex-col  items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sn text-3xl xl:text-xl xl:rounded-full' >
+      {navData.map((link, index) => (
+          <Link
+          className={`${link.path === pathname && 'text-accent'}`}
+           href={link.path} 
+           key={index}>
+            <div>{link.icon}</div>
+          </Link>
+        ))}
     </div>
   </nav>
+  )
   
 };
 
