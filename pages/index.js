a9ba3f351a14588e4a2d187752634b7ce7660e15
Brayan Styvison <styvisonviana@gmail.com>
2023-08-15 20:32:43 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+//next image
+import Image from "next/image";
+
+//componets
+import ParticlesContainer from '../components/ParticlesContainer'
+import ProjectsBtn from '../components/ProjectsBtn'
+import Avatar from '../components/Avatar'
+
 const Home = () => {
   return <div>Home</div>;
 };
