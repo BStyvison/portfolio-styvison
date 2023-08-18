16b8d83fd6fb6e209904534fe171a6f467872c33
Brayan Styvison <styvisonviana@gmail.com>
2023-08-18 14:35:03 -0300

    motion

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import { ProjectsBtn } from '../components/ProjectsBtn'
 import Avatar from '../components/Avatar'
 
 //frame motion
-import { motion } from "framer-motion";
+import { easeInOut, motion } from "framer-motion";
 
 //variants
 import {fadeIn} from '../variants'
@@ -53,7 +53,19 @@ export const Home = () => {
       {/* Image */}
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
         {/* bg img */}
-        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0">
+        </div>
+        {/*particles */}
+        <div>particles</div>
+        {/*avatar img */}
+        <motion.div
+          variants={fadeIn('up', 0.5)} initial="hidden" 
+          animate='show' 
+          exit='hidden' 
+          transition={{duration: 1, ease:easeInOut}}
+          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
+          <Avatar/>
+        </motion.div>
       </div>
     </div>
 
