a1b93ee140f080b8bd79b3a150dfb766e4b5c5bc
Brayan Styvison <styvisonviana@gmail.com>
2023-08-18 15:44:55 -0300

    particles

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -44,9 +44,51 @@ const ParticlesContainer = () => {
           repulse: {
             distance: 200,
             duration: 0.4,
-          },
+          }
+        }
+      },
+      particles: {
+        color: {
+          value: '#e6be2e'
+        },
+        links: {
+          color: '#f5d393',
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1
+        },
+        collisions: {
+          enable: true,
+        },
+        move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+                default: "bounce",
+            },
+            random: false,
+            speed: 1,
+            straight: false,
+        },
+        number: {
+            density: {
+                enable: true,
+                area: 880,
+            },
+            value: 80,
+        },
+        opacity: {
+            value: 0.5,
+        },
+        shape: {
+            type: "circle",
+        },
+        size: {
+            value: { min: 1, max: 5 },
         },
       },
+      detectRetina: true,
     }}>
 
     </Particles>
