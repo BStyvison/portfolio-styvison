9c1c32e1017a18adaa28092ec4e592e286dbc85f
Brayan Styvison <styvisonviana@gmail.com>
2023-08-18 15:09:24 -0300

    Update ParticlesContainer.js

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,5 +1,56 @@
+import {Particles} from 'react-tsparticles'
+import { loadFull } from "tsparticles"
+import React, {useCallback} from 'react';
+
+
 const ParticlesContainer = () => {
-  <div>Particles Container</div>;
+  //init
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine)
+  }, [])
+
+  const particlesLoaded = useCallback(async() => {}, [])
+
+  return (
+    <Particles 
+    className='w-full h-full absolute translate-z-0'
+      id='tsParticles' 
+      init={particlesInit} 
+      loaded={particlesLoaded} 
+      options={{
+        fullScreen: {enable: false},
+        background: {
+          color: {
+            value: '',
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: false,
+            mode: 'push',
+          },
+          onHover: {
+            enable: true,
+            mode: 'repulse'
+          },
+          resize: true,
+        },
+        modes: {
+          push: {
+            quantity: 90
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+    }}>
+
+    </Particles>
+  )
 };
 
 export default ParticlesContainer;
