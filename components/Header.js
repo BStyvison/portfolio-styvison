0760bf7789aafe8ef99495b347cf3767037bb6e8
Brayan Styvison <styvisonviana@gmail.com>
2023-08-19 10:50:36 -0300

    update

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,11 @@
+//next image
+import Image from "next/image";
+
+//next link 
+import Link from "next/link";
+
+
+
 const Header = () => {
   return (
     <header className="absolute z-30 w-full">Header</header>
