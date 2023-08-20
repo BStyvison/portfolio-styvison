e81b3dba8b85cf2743afcd0db08f30b1b20d5180
Brayan Styvison <styvisonviana@gmail.com>
2023-08-19 23:51:16 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export const Home = () => {
     <div className="bg-primary/60 h-full">
       {/*Text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-6 xl:text-left h-full container mx-auto">
+        <div className="text-center flex flex-col justify-center xl:pt-10 xl:text-left h-full container mx-auto">
           {/*Title */}
           <motion.h1    
             variants={fadeIn('down', 0.2)} initial="hidden" 
@@ -25,8 +25,8 @@ export const Home = () => {
             exit='hidden'
             className="h1"
           > 
-            Transforming Ideas <br/> Intro{' '}
-            <span className="text-accent">Digital Reality</span>
+            Arte e tecnologia     <br/> unidas para uma{' '}
+            <span className="text-accent"> <br/> experiência impactante </span>
           </motion.h1>
           {/* Subtitle */}
           <motion.p             
@@ -56,14 +56,14 @@ export const Home = () => {
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0">
         </div>
         {/*particles */}
-        {/*<ParticlesContainer/>*/}
+          <ParticlesContainer/>
         {/*avatar img */}
         <motion.div
           variants={fadeIn('up', 0.5)} initial="hidden" 
           animate='show' 
           exit='hidden' 
           transition={{duration: 1, ease:easeInOut}}
-          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
+          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[2%]">
           <Avatar/>
         </motion.div>
       </div>
