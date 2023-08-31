54545fdc01ce195b959d09cda423c034cfab83cb
Brayan Styvison <styvisonviana@gmail.com>
2023-08-30 23:48:05 -0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,10 @@
-//next image
-import Image from "next/image";
+// next image
+import Image from 'next/image'
 
-//next link 
-import Link from "next/link";
+// next link
+import Link from 'next/link'
 
-//components
+// components
 import Socials from '../components/Socials'
 
 const Header = () => {
@@ -12,18 +12,18 @@ const Header = () => {
     <header className="absolute z-30 w-full flex items-center px-16 xl:h-[90px]" >
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
-          {/*logo */}
+          {/* logo */}
           <Link href={'/'}>
-          <Image 
-            src={'/logo.svg'}
-            width={220}
-            height={48}
-            alt=""
-            priority={true}
-          />
+            <Image
+              src={'/logo.svg'}
+              width={220}
+              height={48}
+              alt=""
+              priority={true}
+            />
           </Link>
-          {/* socials*/}
-          {/*<Socials/>*/}
+          {/* socials */}
+          {/* <Socials/> */}
         </div>
       </div>
     </header>
