8a3c37112a87511f35b9a79dd08eb85de3e91cbb
Brayan Styvison <styvisonviana@gmail.com>
2023-08-31 15:24:26 -0300

    header

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ import Socials from '../components/Socials'
 
 const Header = () => {
   return (
-    <header className="absolute z-30 w-full flex items-center px-16 xl:h-[90px]" >
+    <header className="absolute z-30 w-full flex items-center px-16 xl:h-[90px]">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
           {/* logo */}
@@ -30,4 +30,4 @@ const Header = () => {
   )
 }
 
-export default Header;
+export default Header
