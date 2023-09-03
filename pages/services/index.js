1d0fe09c2ad1363c21c67e7580bd76eaef6f8d54
Brayan Styvison <styvisonviana@gmail.com>
2023-09-03 14:14:12 -0300

    services finishin

--- pages/services/index.js
+++ pages/services/index.js
@@ -57,18 +57,36 @@ const Services = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* text */}
-          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4">
-            <h2 className="h2 xl:mt-8">
-              My Services <span className="text-accent"></span>
-            </h2>
-            <p className="">
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2
+              variants={fadeIn('up', 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8"
+            >
+              My Services <span className="text-accent">.</span>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn('up', 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[480px] mx-auto lg:max-0"
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit
-            </p>
+            </motion.p>
           </div>
-          <div className="w-full xl:max-w-[63%]">
-            {/* slider */}
+          {/* slider */}
+          <motion.div
+            variants={fadeIn('down', 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[63%]"
+          >
             <ServiceSlider />
-          </div>
+          </motion.div>
         </div>
       </div>
       <Bulb />
