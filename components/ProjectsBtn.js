29fe37bf18447b88a5040c88b598350972759306
Brayan Styvison <styvisonviana@gmail.com>
2023-09-04 01:54:25 -0300

    final final

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,29 +1,28 @@
-//next image
-import Image from "next/image";
+// next image
+import Image from 'next/image'
 
-//next link
-import Link from "next/link";
+// next link
+import Link from 'next/link'
 
-//icons
-import { HiArrowRight } from "react-icons/hi2";
+// icons
+import { HiArrowRight } from 'react-icons/hi2'
 
 export const ProjectsBtn = () => {
   return (
-  <div className="mx-auto xl:mx-0">
-    <Link href={'/work'} className="relative w-[175px] h-[175px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group">
-      <Image 
-        src={'/rounded-text.png'}
-        width={141}
-        height={148}
-        alt=""
-        className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
-        
-      />
-      <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
-    </Link>
-  </div>
+    <div className="mx-auto xl:mx-0">
+      <Link
+        href={'/work'}
+        className="relative w-[175px] h-[175px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
+      >
+        <Image
+          src={'/rounded-text.png'}
+          width={141}
+          height={148}
+          alt=""
+          className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
+        />
+        <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
+      </Link>
+    </div>
   )
-
 }
-
-
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export const Home = () => {
     <div className="bg-primary/60 h-full">
       {/* Text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-10 xl:text-left h-full container mx-auto">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* Title */}
           <motion.h1
             variants={fadeIn('down', 0.2)}
@@ -38,7 +38,7 @@ export const Home = () => {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-12"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
           >
             Lorem ipsum dolor sit amet. Cum sint autem qui earum voluptuaries
             sapiente dolor nam nihil temporibus et sint galisum!
